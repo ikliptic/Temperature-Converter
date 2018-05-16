35dcd2b65a086c58c7e23fbcb3b0d0f24959a0cc
ikliptic <adrian@ikliptic.com>
2018-05-16 14:59:48 -0500

    version 1.1

--- app.js
+++ app.js
@@ -9,26 +9,30 @@ function celciusToFarenheitAndKelvin() {
   const cTemp = parseFloat(celciusInput.value);
   const fTemp = (cTemp * (9/5) + 32);
   const kTemp = (cTemp + 273.15);
-  fahrenheitInput.value = fTemp;
-  kelvinInput.value = kTemp;
+  fahrenheitInput.value = roundNum(fTemp);
+  kelvinInput.value = roundNum(kTemp);
 };
 
 function fahrenheitToCelciusAndKelvin() {
   const fTemp = parseFloat(fahrenheitInput.value);
   const cTemp = (fTemp - 32) * (5/9);
   const kTemp = (fTemp + 459.67) * 5/9;
-  celciusInput.value = cTemp;
-  kelvinInput.value = kTemp;
+  celciusInput.value = roundNum(cTemp);
+  kelvinInput.value = roundNum(kTemp);
 };
 
 function kelvinToCelciusAndFarhenheit() {
   const kTemp = parseFloat(kelvinInput.value);
   const cTemp = kTemp - 273.15;
   const fTemp = 9/5 * (kTemp -273) +32;
-  celciusInput.value = cTemp;
-  fahrenheitInput.value = fTemp;
+  celciusInput.value = roundNum(cTemp);
+  fahrenheitInput.value = roundNum(fTemp);
 };
 
+function main() {
 celciusInput.addEventListener('input', celciusToFarenheitAndKelvin);
 fahrenheitInput.addEventListener('input', fahrenheitToCelciusAndKelvin);
 kelvinInput.addEventListener('input', kelvinToCelciusAndFarhenheit);
+};
+
+main();
